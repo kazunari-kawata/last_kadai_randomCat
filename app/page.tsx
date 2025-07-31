980ac03fcd431a1301a4fe2999dc0c75114eef3e
kazunari-kawata <business.kawata@gmail.com>
2025-07-31 21:57:03 +0900

    Update welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { CatImage } from "./cat-image";
 import { fetchImage } from "./fetch-image";
-import { LikeButton } from "./like-button";
 
 export default async function Home() {
     const image = await fetchImage();
